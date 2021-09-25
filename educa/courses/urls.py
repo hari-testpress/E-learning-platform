5803f67aa57a41be84fd535f2382192410d54bfb
hari-testpress <hari@testpress.in>
2021-09-25 14:53:59 +0530

    fix the bug

--- educa/courses/urls.py
+++ educa/courses/urls.py
@@ -9,11 +9,9 @@ urlpatterns = [
         views.ManageCourseListView.as_view(),
         name="manage_course_list",
     ),
-    path("create/", views.CourseCreateView.as_view(), name="course_create"),
-    path("<pk>/edit/", views.CourseUpdateView.as_view(), name="course_edit"),
-    path(
-        "<pk>/delete/", views.CourseDeleteView.as_view(), name="course_delete"
-    ),
+    path("create/", views.CourseCreateView.as_view(), name="create"),
+    path("<pk>/edit/", views.CourseUpdateView.as_view(), name="edit"),
+    path("<pk>/delete/", views.CourseDeleteView.as_view(), name="delete"),
     path(
         "<pk>/module/",
         views.CourseModuleUpdateView.as_view(),
